575acd5b67b8c39dbcd10469975f00c5d475b3f1
rodalvis-sys <85137322+rodalvis-sys@users.noreply.github.com>
2022-03-29 22:17:13 -0400

    Delete Tarea8web directory

--- Tarea8web/api/lib/getLocation.php
+++
@@ -1,7 +0,0 @@
-<?php
-function getLocation($lat, $long){
-    $url = "http://api.geonames.org/countryCodeJSON?lat=" . $lat . "&lng=" . $long . "&username=prueba";
-    $json = @file_get_contents($url);
-    $data = json_decode($json);
-    return $data;
-}
--- Tarea8web/api/lib/message.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function sendResponse($message, $code){
-    http_response_code($code);
-    echo json_encode(array("message"=>$message));
-}
